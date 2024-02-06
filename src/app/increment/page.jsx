c0dc4102472ad8666df2077530083b0686bbe688
Sudip <sudippanta738@gmail.com>
2024-02-06 12:09:57 +0545

    button

---
+++ src/app/increment/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+export default function IncrementPage() {
+
+        const[count, setCount] = useState(0);
+
+        function increaseValue(){
+            setCount(count + 1);
+        }
+        function resetValue(){
+            setCount(0);
+        }
+
+  return (
+    <div className="flex w-full min-h-screen justify-center items-center flex-col gap-4">
+        <h1 className="font-semibold text-blue-500">Counter Increment App</h1>
+
+        <p className="text-xl font-bold text-blue-800">{count}</p>
+        
+        <button onClick={increaseValue} className="bg-green-500 text-white px-5 py-2 rounded-full hover:bg-green-300">Increment</button>
+        
+        <button onClick={resetValue} className= "bg-red-500 text-white  px-5 py-2 rounded-full hover:bg-red-300">Reset</button>
+    </div>
+  )
+}
